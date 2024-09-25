2c57ff4c7f5d224c623aa4ee8ea37562a46452bc
Jesus Bravo <jd_bravo24@hotmail.com>
2024-09-25 17:37:36 -0500

    config

--- src/plugins/exporter/server/config/index.js
+++ src/plugins/exporter/server/config/index.js
@@ -1,6 +1,25 @@
 'use strict';
 
 module.exports = {
-  default: {},
-  validator() {},
+  default: {
+    exportFormat: 'csv',
+    exportPath: './exports/',
+    maxRecords: 1000,
+  },
+  validator(config) {
+    // Validar que el formato sea csv o json
+    if (!['csv', 'json'].includes(config.exportFormat)) {
+      throw new Error('El formato de exportación debe ser "csv" o "json".');
+    }
+
+    // Validar que la ruta de exportación esté definida
+    if (typeof config.exportPath !== 'string' || !config.exportPath) {
+      throw new Error('Debes especificar una ruta válida para exportar los archivos.');
+    }
+
+    // Validar que maxRecords sea un número positivo
+    if (typeof config.maxRecords !== 'number' || config.maxRecords <= 0) {
+      throw new Error('maxRecords debe ser un número positivo.');
+    }
+  },
 };
